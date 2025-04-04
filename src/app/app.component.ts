f059ba1b69ab7b05acf56f80151a8ede29551ad4
matheushssilvaa <msxatendimento@outlook.com.br>
2025-04-04 14:52:41 -0300

    project Angular Blog formation DIO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
+            
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  imports: [],
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover: string = ""
+
+  @Input()
+  cardTitle: string = ""
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+}
